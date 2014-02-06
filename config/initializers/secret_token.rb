d540b067b0087d9f8e7e324799e9123b8935db51
dani martin <daniel@martinlombardo.com>
2014-02-06 16:43:01 +0100

    inicio

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Danipin::Application.config.secret_key_base = '027263e321cf0a596260260b83b04538ecd50b4f798a7142d1315baa273e29835c78da15e9420904f8ee2bccaea75080914e19a7dcb2ca3a4b565318d12eceb5'
